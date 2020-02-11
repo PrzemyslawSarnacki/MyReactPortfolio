5616ee102b8faa78ed89d83b81f954fee0a92731
PrzemyslawSarnacki <przemos100@gmail.com>
2020-02-11 22:50:37 +0100

    First commit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    "imagebaseurl":"https://przemyslawsarnacki.github.io/",
+    "name": "Przemyslaw Sarnacki",
+    "role": "Backend Developer",
+    "linkedinId":" PrzemyslawSarnacki",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of backend development and like to learn about new technologies. My another passion is having fun with IoT devices. In my free time I'm involved in Powerlifting.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/PrzemyslawSarnacki",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com/bboy_przemko",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a last year student at The Military University of Technology (Warsaw). I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Poland",
+    "website":"https://przemyslawsarnacki.github.io",
+    "education":[
+      {
+        "UniversityName":"Military University of Technology",
+        "specialization":"Information and Communication Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Articles"
+      },
+      {
+        "UniversityName":"I High School",
+        "specialization":"Math and Physics",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2016",
+        "Achievements":"laureate of some science competitions"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"JS Product",
+        "specialization":"Technical support and CCTV",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Ministry of National Defence",
+        "specialization":"Military Project",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"My skills described below",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Security Survey",
+        "description":"Proggresive Web App made mostly in Django in order to help you with choice of appropriate security degree of your building",
+        "imgurl":"images/portfolio/security_screenshot.png"
+      },
+      {
+        "name":"DjangoFurnitureShop",
+        "description":"My first Django E-Commerce",  
+        "imgurl":"images/portfolio/screenshot_furniture_shop.png"
+      },
+      {
+        "name":"Waspmote Flutter BLE app",
+        "description":"App designed for a Waspmote Laboratory with use of Bluetooth Low Energy",
+        "imgurl":"images/portfolio/screenshot_flutter_ble.png"
+      },
+      {
+        "name":"invoiceApp",
+        "description":"Program which allows you to generate Invoices which uses MongoDB and Qt",
+        "imgurl":"images/portfolio/screenshot_invoice_app.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"“It has become appallingly obvious that our technology has exceeded our humanity.”",
+        "name":"Albert Einstain"
+      },
+      {
+        "description":"“Once a new technology rolls over you, if you're not part of the steamroller, you're part of the road.”",
+        "name":"Stewart Brand"
+      },
+      {
+        "description":" “It's not a faith in technology. It's faith in people.”",
+        "name":"Steve Jobs"
+      }
+    ]
+  }
+  
+  export default resumeData
